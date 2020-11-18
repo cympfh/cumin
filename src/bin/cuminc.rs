d90943f6027577bea5787b7512a67373801b7b60
/c/ympfh <cympfh@gmail.com>
2020-11-18 21:36:43 +0900

    cuminc can read from stdin (#20)

--- src/bin/cuminc.rs
+++ src/bin/cuminc.rs
@@ -10,18 +10,25 @@ struct Opt {
     #[structopt(short = "T", long = "type", default_value = "json")]
     output_type: String,
 
-    #[structopt(name = "INPUT")]
+    #[structopt(name = "INPUT", default_value = "-")]
     input_cumin: String,
 }
 
 fn cat(file_name: String) -> String {
     use std::fs::File;
-    use std::io::prelude::*;
     use std::io::BufReader;
-    let file = File::open(&file_name).unwrap();
-    let mut buf_reader = BufReader::new(file);
+    use std::io::{self, Read};
+
     let mut content = String::new();
-    buf_reader.read_to_string(&mut content).unwrap();
+    if file_name == "-" {
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        handle.read_to_string(&mut content).unwrap();
+    } else {
+        let file = File::open(&file_name).unwrap();
+        let mut buf_reader = BufReader::new(file);
+        buf_reader.read_to_string(&mut content).unwrap();
+    }
     content
 }
 
