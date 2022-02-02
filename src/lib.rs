3006c0e85c2b9ed1c9016ff6d60ccefb7681cf8f
/c/ympfh <cympfh@gmail.com>
2022-02-03 01:42:10 +0900

    Clean/wasm (#82)
    * wasm works!
    
    * Returns JSON or Error Message
    
    * Update wasm -> 0.9.9a
    
    * update
    
    * update
    
    * Update README.md
    
    centering
    
    * update -> v0.9.12
    
    * :fire: rename export function name: load -> compile
    
    * clean up

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,26 @@ extern crate nom;
 pub mod builtins;
 pub mod errors;
 pub mod eval;
+pub mod eval_wasm;
 pub mod json;
 pub mod parser;
+
+use crate::{eval_wasm::eval_wasm, parser::cumin::cumin};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn compile(input: &str) -> String {
+    match cumin(input) {
+        Ok((rest, data)) => {
+            if rest == "" {
+                match eval_wasm(data) {
+                    Ok(data) => data.stringify(),
+                    Err(err) => format!("Error: eval failed ({:?})", err),
+                }
+            } else {
+                format!("Error: Parsing stopped at {}", &rest)
+            }
+        }
+        Err(err) => format!("Error: Parsing failed ({:?})", &err),
+    }
+}
